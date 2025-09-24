94f258b0a63b86c9bb899242ed291dfcb95a0511
rochelle13 <rochellemoodley13@gmail.com>
2025-09-24 19:01:57 +0200

    Login and Registration using Firebase Authentication

---
+++ app/src/main/java/com/rochelle/rochrun/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.rochelle.rochrun
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var etEmail: EditText
+    lateinit var etConfPass: EditText
+    private lateinit var etPass: EditText
+    private lateinit var btnSignUp: Button
+    lateinit var tvRedirectLogin: TextView
+
+    // create Firebase authentication object
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+        }
+        // View Bindings
+        etEmail = findViewById(R.id.etSEmailAddress)
+        etConfPass = findViewById(R.id.etSConfPassword)
+        etPass = findViewById(R.id.etSPassword)
+        btnSignUp = findViewById(R.id.btnSigned)
+        tvRedirectLogin = findViewById(R.id.tvRedirectLogin)
+
+        // Initialising auth object
+        auth = FirebaseAuth.getInstance()
+
+        btnSignUp.setOnClickListener {
+            signUpUser()
+        }
+
+        // switching from signUp Activity to Login Activity
+        tvRedirectLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun signUpUser() {
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+        val confirmPassword = etConfPass.text.toString()
+
+        // check pass
+        if (email.isBlank() || pass.isBlank() || confirmPassword.isBlank()) {
+            Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (pass != confirmPassword) {
+            Toast.makeText(this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        // If all credential are correct
+        // We call createUserWithEmailAndPassword
+        // using auth object and pass the
+        // email and pass in it.
+        val intent = Intent(this, LoginActivity::class.java)
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Successfully Signed Up", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Sign Up Failed!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+}
